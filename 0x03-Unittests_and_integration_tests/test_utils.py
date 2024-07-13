c9db15426aaeaad90788f9fc37c1c1e6e1033a14
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-07-14 01:35:10 +0300

    0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""the unit test for utils.access_nested_map"""
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch
+import unittest
+# import requests
+from parameterized import parameterized
+from typing import Mapping, Sequence, Any, Dict
+
+class TestAccessNestedMap(unittest.TestCase):
+    """test access nested map"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: Any):
+        """testing method"""
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
